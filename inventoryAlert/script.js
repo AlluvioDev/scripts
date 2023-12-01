5a5893c15f0e4d1bd3023d4f29fbc01a32e7640e
4eDo <61490588+4eDo@users.noreply.github.com>
2023-12-01 11:33:22 +0300

    Update script.js

--- inventoryAlert/script.js
+++ inventoryAlert/script.js
@@ -1,4 +1,4 @@
-const version = "v2.11"; // Обнови меня, если меняешь код!
+const version = "v2.12"; // Обнови меня, если меняешь код!
 
 const DEBUG_MODE = true; // true - уведомление никогда не исчезает, false  - всё работает в нормальном режиме.
 const UPDATE_INTERVAL_IN_MS = 120_000; //120_000 (2 min) | 3_600_000 (1h) | 43_200_000 (12h) | 86_400_000 (24h)
@@ -228,7 +228,9 @@ function closeAlertWindow() {
 	$('body').remove("." + MESSAGE_PANEL_WRAPPER_CLASS);
 }
 showAlertIfInventoryChanged();
-$('.' + MESSAGE_CLOSE_BTN_CLASS).on("click", ".menu_button", function() { closeAlertWindow()});
+document.querySelectorAll('.' + MESSAGE_CLOSE_BTN_CLASS).forEach(el => el.addEventListener('click', event => {
+	closeAlertWindow();
+}));
 
 function getItems(oldItemsString, newItemsString) {
 	let oldItemsArr = oldItemsString ? splitItemsStringToArr(oldItemsString.replace(/[\r\n\t]+/g, '').trim()) : [];
