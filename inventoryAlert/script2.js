7735efca4366267761199c39b5b47c246b919baa
4eDo <61490588+4eDo@users.noreply.github.com>
2023-12-14 21:56:16 +0300

    Update script2.js

--- inventoryAlert/script2.js
+++ inventoryAlert/script2.js
@@ -1,4 +1,4 @@
-const version = "v4.11"; // Обнови меня, если меняешь код!
+const version = "v4.12"; // Обнови меня, если меняешь код!
 
 const DEBUG_MODE = false; // true - уведомление никогда не исчезает, false  - всё работает в нормальном режиме.
 const UPDATE_INTERVAL_IN_MS = 120_000; //120_000 (2 min) | 3_600_000 (1h) | 43_200_000 (12h) | 86_400_000 (24h)
@@ -113,7 +113,7 @@ function saveCurrentInventory() {
 				setValueToStorage(i + "_" + jId, items.slice(j, j+CHUNK_SIZE ).join("\n"));
 				jId++;
 			} else {
-				setValueToStorage(i, invStr[i]);
+				setValueToStorage(i, invStr[i].join("\n"));
 			}
 		}
 	}
