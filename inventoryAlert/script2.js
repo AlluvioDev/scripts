ed5f60681c4847f9079a04d9bfb826a4847eb8cd
4eDo <61490588+4eDo@users.noreply.github.com>
2023-12-14 22:33:49 +0300

    Update script2.js

--- inventoryAlert/script2.js
+++ inventoryAlert/script2.js
@@ -1,4 +1,4 @@
-const version = "v4.13"; // Обнови меня, если меняешь код!
+const version = "v4.14"; // Обнови меня, если меняешь код!
 
 const DEBUG_MODE = false; // true - уведомление никогда не исчезает, false  - всё работает в нормальном режиме.
 const UPDATE_INTERVAL_IN_MS = 120_000; //120_000 (2 min) | 3_600_000 (1h) | 43_200_000 (12h) | 86_400_000 (24h)
@@ -106,7 +106,7 @@ function saveCurrentInventory() {
 	let current = new Date();
 	current = current.getTime();
 	setValueToStorage(0, current);
-	for(let i = 0; i < 6; i++) {
+	for(let i = 1; i < 6; i++) {
 		let items = splitItemsStringToArr(invStr[i]);
 		let chunksCount = Math.floor(items.length / CHUNK_SIZE);
 		let jId = 1;
