30fb4f76032f788bbe496628aa1a96a74576bc0b
4eDo <61490588+4eDo@users.noreply.github.com>
2023-12-14 20:48:57 +0300

    Update script2.js

--- inventoryAlert/script2.js
+++ inventoryAlert/script2.js
@@ -1,4 +1,4 @@
-const version = "v4.03"; // Обнови меня, если меняешь код!
+const version = "v4.04"; // Обнови меня, если меняешь код!
 
 const DEBUG_MODE = false; // true - уведомление никогда не исчезает, false  - всё работает в нормальном режиме.
 const UPDATE_INTERVAL_IN_MS = 120_000; //120_000 (2 min) | 3_600_000 (1h) | 43_200_000 (12h) | 86_400_000 (24h)
@@ -137,6 +137,8 @@ function setValueToStorage(keySuffix, keyValue){
 		},
 		  error: function(){
 		    	console.log("Error on setValueToStorage method [2]");
+		    	console.log(keySuffix);
+		    	console.log(keyValue);
 		  }
 	});
 }
@@ -180,6 +182,7 @@ function getValueFromStorage(keySuffix){
 		},
 		  error: function(){
 		    	console.log("Error on getValueFromStorage method [2]");
+		    	console.log(keySuffix);
 		  }
 	});
 	return valueFromStorage;
