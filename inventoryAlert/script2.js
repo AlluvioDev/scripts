c4cd91a76e854ce70514854f27076af37c7f5a26
4eDo <61490588+4eDo@users.noreply.github.com>
2023-12-14 23:11:33 +0300

    Update script2.js

--- inventoryAlert/script2.js
+++ inventoryAlert/script2.js
@@ -1,4 +1,4 @@
-const version = "v4.16"; // Обнови меня, если меняешь код!
+const version = "v4.17"; // Обнови меня, если меняешь код!
 
 const DEBUG_MODE = false; // true - уведомление никогда не исчезает, false  - всё работает в нормальном режиме.
 const UPDATE_INTERVAL_IN_MS = 120_000; //120_000 (2 min) | 3_600_000 (1h) | 43_200_000 (12h) | 86_400_000 (24h)
@@ -107,7 +107,7 @@ function saveCurrentInventory() {
 	current = current.getTime();
 	setValueToStorage(0, current);
 	for(let i = 1; i < 6; i++) {
-		let items = splitItemsStringToArr(invStr[i]);
+		let items = invStr[i];
 		let chunksCount = Math.floor(items.length / CHUNK_SIZE);
 		let jId = 1;
 		for(let j = 0; j <= items.length; j = j + CHUNK_SIZE){
@@ -122,7 +122,7 @@ function saveCurrentInventory() {
 	}
 }
 function setValueToStorage(keySuffix, keyValue){
-	if(keyValue  instanceof Array) {
+	if(keyValue instanceof Array) {
 		keyValue = keyValue.join("\n");
 	}
 	$.ajax({
