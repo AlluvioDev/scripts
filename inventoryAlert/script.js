a2a67dc9cf0e3cac107aa8c5b318891352ccdf64
4eDo <61490588+4eDo@users.noreply.github.com>
2023-12-15 00:28:19 +0300

    Update script.js

--- inventoryAlert/script.js
+++ inventoryAlert/script.js
@@ -1,4 +1,4 @@
-const version = "v4.20"; // Обнови меня, если меняешь код!
+const version = "v4.21"; // Обнови меня, если меняешь код!
 
 const DEBUG_MODE = false; // true - уведомление никогда не исчезает, false  - всё работает в нормальном режиме.
 const UPDATE_INTERVAL_IN_MS = 120_000; //120_000 (2 min) | 3_600_000 (1h) | 43_200_000 (12h) | 86_400_000 (24h)
@@ -343,7 +343,7 @@ function getItems(oldItemsArr, newItemsArr) {
 
 function splitItemsStringToArr(istr) {
 	let arr = [];
-	if(this.length === 0 || !this.trim()) return arr;
+	if(istr.length === 0 || !istr.trim()) return arr;
 	$(istr).filter('img').each(function(idx, ctx) {arr.push(ctx.outerHTML.toString())});
 	return arr;
 }
