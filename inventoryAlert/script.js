ed42e0b9a69155cdc714283c86129e870528d690
4eDo <61490588+4eDo@users.noreply.github.com>
2023-12-17 22:50:14 +0300

    Update script.js

--- inventoryAlert/script.js
+++ inventoryAlert/script.js
@@ -1,4 +1,4 @@
-const version = "v4.24"; // Обнови меня, если меняешь код!
+const version = "v4.25"; // Обнови меня, если меняешь код!
 
 const DEBUG_MODE = false; // true - уведомление никогда не исчезает, false  - всё работает в нормальном режиме.
 const UPDATE_INTERVAL_IN_MS = 120_000; //120_000 (2 min) | 3_600_000 (1h) | 43_200_000 (12h) | 86_400_000 (24h)
@@ -308,8 +308,8 @@ function getItems(oldItemsArr, newItemsArr) {
 	// let oldItemsArr = oldItemsString ? splitItemsStringToArr(oldItemsString.replace(/[\r\n\t]+/g, '').trim()) : [];
 	// let newItemsArr = newItemsString ? splitItemsStringToArr(newItemsString.replace(/[\r\n\t]+/g, '').trim()) : [];
 
-	let cleanedOldArr = oldItemsArr.map(function(x){ return x.replace(regexpSpecChars, ""); });
-	let cleanedNewArr = newItemsArr.map(function(x){ return x.replace(regexpSpecChars, ""); });
+	let cleanedOldArr = oldItemsArr.map(function(x){ return x.replace(regexpSpecChars, "").trim(); });
+	let cleanedNewArr = newItemsArr.map(function(x){ return x.replace(regexpSpecChars, "").trim(); });
 	
 	var result = [];
 	
