d3ac99b4abe14b0bf560ad05694db7f690dbe403
4eDo <61490588+4eDo@users.noreply.github.com>
2023-12-17 22:11:28 +0300

    Update script.js

--- inventoryAlert/script.js
+++ inventoryAlert/script.js
@@ -289,7 +289,7 @@ function showAlertIfInventoryChanged() {
 	// 	console.log("message:" + message);
 	// 	return 0;
 	// }
-	if(message.length > 1) {
+	if(message.length > 1 && UserID != 54) {
 		$('body').append(MESSAGE_PANEL_WRAPPER_START + MESSAGE_CLOSE_BTN + message + MESSAGE_PANEL_WRAPPER_END);
 	}
 	
